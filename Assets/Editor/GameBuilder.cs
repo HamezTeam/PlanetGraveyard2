81522eb8c703e3ce69b05ba6de83f2cffbb270e6
dimakill3 <55401003+dimakill3@users.noreply.github.com>
2023-05-15 15:07:17 +0300

    GameBuilder class

---
+++ Assets/Editor/GameBuilder.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Platformer
+{
+    public class GameBuilder : MonoBehaviour
+    {
+        [MenuItem("Build/Build macOS")]
+        public static void PerformMacOSBuild()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+            buildPlayerOptions.locationPathName = "build/macOS/PlanetGraveyard2.app";
+            buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+            
+            if (summary.result == BuildResult.Succeeded)
+                Debug.Log("Build succeeded" + summary.totalSize + "bytes");
+            
+            if (summary.result == BuildResult.Failed)
+                Debug.Log("Build failed");
+        }
+    }
+}
