d34e0b07304e54be71e9dc90140aa8aa71a030cf
dimakill3 <55401003+dimakill3@users.noreply.github.com>
2023-05-21 23:01:50 +0300

    WebGL Build

--- Assets/Editor/GameBuilder.cs
+++ Assets/Editor/GameBuilder.cs
@@ -24,5 +24,24 @@ namespace Platformer
             if (summary.result == BuildResult.Failed)
                 Debug.Log("Build failed");
         }
+        
+        [MenuItem("Build/Build macOS")]
+        public static void PerformWebGLBuild()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+            buildPlayerOptions.locationPathName = "build/WebGL";
+            buildPlayerOptions.target = BuildTarget.WebGL;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+            
+            if (summary.result == BuildResult.Succeeded)
+                Debug.Log("Build succeeded" + summary.totalSize + "bytes");
+            
+            if (summary.result == BuildResult.Failed)
+                Debug.Log("Build failed");
+        }
     }
 }
